c313a17bd7d2b5ef9a393e24834c95f8e1dfef43
Marko Matoničkin <marko@matonickin.com>
2024-11-05 23:38:29 +0100

    Fixed lap time counter and added --no_render argument

--- run.py
+++ run.py
@@ -8,26 +8,27 @@ from config import state_config
 MAP_ROI = 120.  # meters
 
 
-def run_mission(map_path: str | Path, logdir: Path | None):
+def run_mission(map_path: str | Path, logdir: Path | None, render: bool):
     state = State(map_path, state_config)
     mission = MyMission()
     sim_runtime = make_simulation_object(state, mission)
-    renderer = StateRenderer()
-    renderer.set_state(state)
+    if render: renderer = StateRenderer()
+    if render: renderer.set_state(state)
     finish_time = None
     for i, (mission_time, observations, mission_action) in enumerate(sim_runtime):
         speed_setpoint = mission_action[1]
         printout = f"Time: {mission_time: .2f}\nSpeed: {state.speed: .2f}\nSetpoint: {speed_setpoint: .2f}\n"
-        renderer.render_state(printout, path=mission_action[3]["path"])
+        if render: renderer.render_state(printout, path=mission_action[3]["path"])
         finished, steering_angle, speed_setpoint, _ = mission_action
         if finished == True:
-            print(f"Finished in {mission_time:.2f} seconds!")
-            finish_time = mission_time
+            lap_time = mission_action[3]["lap_times"][0]
+            print(f"Finished lap in {lap_time:.2f} seconds!")
+            finish_time = lap_time
             break
         elif not state.is_within_roi(MAP_ROI):
             print("Car went out of bounds!")
             break
-    renderer.close()
+    if render: renderer.close()
     if logdir is not None:
         history_to_csv(state, logdir / "history.csv")
     plot_state_summary_and_wait(state, finish_time, success=finish_time is not None)
@@ -36,6 +37,7 @@ def run_mission(map_path: str | Path, logdir: Path | None):
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("--no_logs", action="store_true", default=False)
+    parser.add_argument("--no_render", action="store_true", default=False)
     args = parser.parse_args()
     if args.no_logs:
         logdir = None
@@ -43,6 +45,10 @@ if __name__ == '__main__':
     else:
         logdir = Path("logs") / datetime.datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
         print(f"Logging to {logdir}")
+    if args.no_render:
+        render = False
+    else:
+        render = True
     for i in range(1, 4):
         MAP_PATH = Path(f"maps/map{i}.json")
         if logdir is not None:
@@ -51,4 +57,4 @@ if __name__ == '__main__':
         else:
             subdir = None
         print(f"Running mission on map {i}")
-        run_mission(MAP_PATH, subdir)
+        run_mission(MAP_PATH, subdir, render)
